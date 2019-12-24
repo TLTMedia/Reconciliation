8f0f9d56d291e6bf1f9e69c9c4468143ceab0934
Paul St. Denis <pstdenis@huey-tll.labs.stonybrook.edu>
2019-12-24 12:54:28 -0500

    Pull Intro from Google Spreadsheet

--- js/Modules/InterfaceController.js
+++ js/Modules/InterfaceController.js
@@ -9,7 +9,6 @@ export class InterfaceController {
         //this.toast = toast;
         //this.student_data = student_data;
         //this.patient_data = patient_data;
-//	this.welcome_text=`Welcome to the medication reconcillation simulation. Each of these exercises will present you with a clinical vignette and provide you with direction to make changes to medication orders.\nYou will get two tries, and your responses will be locked in afterwards.\nThe purpose is to create a EHR agnostic experience that will hopefully help you learn how to make sure you order medications correctly when you start your internship. Good luck!` //intro_data.replace("\n","<br>")
 
         /**
          * String constants
