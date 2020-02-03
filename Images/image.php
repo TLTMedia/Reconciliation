acb513d0213a7bdda5bf8265225d2231c60a0a5e
shortland <ilankleiman@gmail.com>
2020-02-03 11:38:30 -0500

    maint: rename images dir, properly export images as PNGs

--- Images/image.php
+++
@@ -1,7 +0,0 @@
-<?
-header('Content-Type: image/png');
-$file=$_GET["image"];
-if(!file_exists($file)){$file="default.png";}
-//print($file);
-print_r(file_get_contents($file));
-?>
---
+++ images/image.php
@@ -0,0 +1,9 @@
+<?php
+// header('Content-Type: image/png');
+
+// $file = $_GET["image"];
+// if (!file_exists($file)) {
+//     $file = "default.png";
+// }
+
+// print_r(file_get_contents($file));
